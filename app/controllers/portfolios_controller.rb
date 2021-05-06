a7fb1de89c75acba85f1e50b9f8afe41d2afe8a7
sipic <nediljko.sipic@thespian.hr>
2021-05-06 13:19:31 +0200

    destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,14 @@ class PortfoliosController < ApplicationController
     def show
     	@portfolio_item = Portfolio.find(params[:id])
     end
+
+    def destroy
+	    @portfolio_item = Portfolio.find(params[:id])
+	    @portfolio_item.destroy
+	    respond_to do |format|
+	      format.html { redirect_to portfolios_url, notice: "Post was successfully destroyed." }
+	      format.json { head :no_content }
+	    end
+  end
+
 end
