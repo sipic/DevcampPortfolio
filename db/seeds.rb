bb8ec6eca40cb4472ab2be7e68d3743a11ab8f06
sipic <nediljko.sipic@thespian.hr>
2021-05-07 10:02:37 +0200

    implemented custom scopres for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}")
+end
 10.times do |blog|
 	Blog.create!(
       title: "My Blog Post #{blog + 1}",
@@ -10,8 +14,8 @@
       quis nostrum exercitationem 
       ullam corporis suscipit laboriosam, 
       nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-       vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-		)
+       vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+       topic_id: Topic.last.id)
 end
 
 puts "10 blog posts created"
@@ -25,10 +29,27 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My great service",
+		subtitle: "Ruby on Rails",
+		body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+		doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
+		et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia 
+		voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+		 ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor 
+		 sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut 
+		 labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+		 exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+		 Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+		  vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		main_image: "https://place-hold.it/600x200",
+		thumb_image: "https://place-hold.it/350x200",)
+end
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",
 		body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
 		doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
 		et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia 
