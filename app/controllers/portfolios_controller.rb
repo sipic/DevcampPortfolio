08c53cb3cf343e57189986fb36bae95d08d94006
sipic <nediljko.sipic@thespian.hr>
2021-05-10 08:52:16 +0200

    refactored params for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,10 +2,10 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
-    
-  def angular
-  	@angular_portfolio_items = Portfolio.angular
-  end
+
+	def angular
+		@angular_portfolio_items = Portfolio.angular
+	end
 
 	def new
 		@portfolio_item = Portfolio.new
@@ -13,7 +13,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+		@portfolio_item = Portfolio.new(portfolio_params)
 
 		respond_to do |format|
 			if @portfolio_item.save
@@ -31,25 +31,34 @@ class PortfoliosController < ApplicationController
 	def update
 	  @portfolio_item = Portfolio.find(params[:id])
 	    respond_to do |format|
-	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
 	      else
 	        format.html { render :edit, status: :unprocessable_entity }
 	      end
-        end
-    end
+	    end
+	end
 
-    def show
-    	@portfolio_item = Portfolio.find(params[:id])
-    end
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
 
-    def destroy
+	def destroy
 	    @portfolio_item = Portfolio.find(params[:id])
 	    @portfolio_item.destroy
 	    respond_to do |format|
 	      format.html { redirect_to portfolios_url, notice: "Post was successfully destroyed." }
 	      format.json { head :no_content }
 	    end
-  end
+	end
+  
+  private
 
+  def portfolio_params
+  	params.require(:portfolio).permit(:title, 
+  		                                :subtitle, 
+  		                                :body, 
+  		                                technologies_attributes: [:name]
+  		                                )
+  end
 end
